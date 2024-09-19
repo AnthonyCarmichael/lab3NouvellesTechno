3c572f05ae138225fab4b7006687cdff2fa3fb7e
Anthony Carmichael <anthonycarmichael0@gmail.com>
2024-09-19 09:59:35 -0400

    Entrer plat

--- lab3NewTech/Source.cpp
+++ lab3NewTech/Source.cpp
@@ -6,8 +6,17 @@ using namespace std;
 int main()
 {
 	string nom = "";
-	cout << "Hello world" << endl << "Entrer votre nom";
-	system("pause");
+	string pref = "";
+	int age = 0;
+
+	cout << "Hello world" << endl << "Entrer votre nom ";
 	cin >> nom;
+
+	cout << "Entrer votre age ";
+	cin >> age;
+
+	cout << "Entrer votre plat favoris ";
+	cin >> pref;
+
 	return 0;
 }
